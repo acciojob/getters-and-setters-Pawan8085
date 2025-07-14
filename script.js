e609f8ddb1eed071db7143410701ad3d9e93bb26
Pawan Kumar <101393436+Pawan8085@users.noreply.github.com>
2025-07-14 17:14:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ class Person {
 	setage(age){
 		this.age = age;
 	}
+
+	study(){
+		console.log(this.name, 'is studying');
+	}
+	teach(){
+		console.log(this.name, 'is teaching');
+	}
 }
 
 class Student extends Person {
@@ -23,6 +30,7 @@ class Student extends Person {
 	study(){
 		console.log(this.name, 'is studying');
 	}
+	
 }
 
 class Teacher extends Person {
@@ -40,20 +48,4 @@ class Teacher extends Person {
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
-
-// Creating a Person instance
-const person = new Person("John", 25);
-console.log(person.name);  // Output: John
-
-person.age = 30;  // Using the setter to change the age
-console.log(person.age);  // Output: 30
-
-// Creating a Student instance
-const student = new Student("Alice", 22);
-student.study();  // Output: Alice is studying
-
-// Creating a Teacher instance
-const teacher = new Teacher("Bob", 40);
-teacher.teach();  // Output: Bob is teaching
-
 
