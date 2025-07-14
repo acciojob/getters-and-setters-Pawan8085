6474c98186d261b1eba8995dcca18e392866eecb
Pawan Kumar <101393436+Pawan8085@users.noreply.github.com>
2025-07-14 16:42:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this.name = name;
+		this.age = age;
+	}
 
-class Student extends Person {}
+	getname(){
+		return this.name;
+	}
+	setage(age){
+		this.age = age;
+	}
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+
+	constructor(name, age){
+		this.name = name;
+		this.age = age;
+	}
+
+	study(){
+		console.log(this.name, 'is studying');
+	}
+}
+
+class Teacher extends Person {
+	constructor(name, age){
+		this.name = name;
+		this.age = age;
+	}
+	teach(){
+		console.log(this.name, 'is teaching');
+	}
+}
+
 
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
+
+
